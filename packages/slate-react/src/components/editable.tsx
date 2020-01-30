1d84651e792db6c2d879fccd7899fb26b4b4d97f
Brian Krausz <briankrausz@gmail.com>
2020-01-30 00:44:49 -0800

    Handle scroll when cursor moves outside of horizontal bounds

--- packages/slate-react/src/components/editable.tsx
+++ packages/slate-react/src/components/editable.tsx
@@ -206,6 +206,15 @@ export const Editable = (props: EditableProps) => {
         )
       }
       const leafEl = newDomRange.startContainer.parentElement!
+
+      const cursor = newDomRange.getBoundingClientRect()
+      const input = el.getBoundingClientRect()
+      if (cursor.left < input.left) {
+        el.scrollBy(cursor.left - input.left, 0)
+      } else if (cursor.right > input.right) {
+        el.scrollBy(cursor.right - input.right + 1, 0)
+      }
+
       scrollIntoView(leafEl, {
         scrollMode: 'if-needed',
         boundary: el,
